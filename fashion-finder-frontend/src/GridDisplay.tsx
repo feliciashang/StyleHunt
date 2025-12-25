c64610cd38c46e2dbad991ebbee38a6431b9dd35
Felicia Shang <feliciashang@gmail.com>
2025-12-24 22:53:35 -0500

    add fallback image

--- fashion-finder-frontend/src/GridDisplay.tsx
+++ fashion-finder-frontend/src/GridDisplay.tsx
@@ -2,38 +2,40 @@ import React from "react";
 import './GridDisplay.css'
 
 interface Props {
-    items: [string,string][];
+    items: [string, string][];
     similarItems: [string, string][];
 }
 
 const GridDisplay: React.FC<Props> = ({ items, similarItems }) => {
     return (
         <div>
-        <p className="subtitle">Fully matching pages</p>
-        <div className="grid grid-cols-3 gap-4 mt-8">
-            {items.map(([imageUrl, pageUrl], index) => (
-                <div key={index} className="grid-item">
-                    <a href={pageUrl} target="_blank" rel="noopener noreferrer">
-                        <img
-                            src={imageUrl}
-                            className="w-full h-auto object-contain rounded-lg border"
+            <p className="subtitle">Fully matching pages</p>
+            <div className="grid grid-cols-3 gap-4 mt-8">
+                {items.map(([imageUrl, pageUrl], index) => (
+                    <div key={index} className="grid-item">
+                        <a href={pageUrl} target="_blank" rel="noopener noreferrer">
+                            <img
+                                src={imageUrl}
+                                className="w-full h-auto object-contain rounded-lg border"
+                            />
+                        </a>
+                    </div>
+                ))}
+            </div>
+            <p className="subtitle">Similar matching pages</p>
+            <div className="grid grid-cols-3 gap-4 mt-8">
+                {similarItems.map(([imageUrl, pageUrl], index) => (
+                    <div key={index}>
+                        <a href={pageUrl} target="_blank" rel="noopener noreferrer">
+                            <img src={imageUrl}
+                                className="w-full h-auto object-contain rounded-lg border"
+                                onError={(e: React.SyntheticEvent<HTMLImageElement, Event>) => {
+                                    (e.target as HTMLImageElement).src = '/image-fallback.jpg'}}
                         />
-                    </a>
-                </div>
-            ))}
-        </div>
-        <p className="subtitle">Similar matching pages</p>
-        <div className="grid grid-cols-3 gap-4 mt-8">
-            {similarItems.map(([imageUrl, pageUrl], index) => (
-                <div key={index}>
-                    <a href={pageUrl} target="_blank" rel="noopener noreferrer">
-                        <img src={imageUrl} 
-                        className="w-full h-auto object-contain rounded-lg border"
-                        />
-                    </a>
-                </div>
-            ))}
-         </div>
+                        </a>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
